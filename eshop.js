df3398211532f06327ddbd5ff4cccba9a513cdae
FeliciaMarlove <missflorencemary@hotmail.com>
2020-01-11 15:37:11 +0100

    Tsunami

--- eshop.js
+++ eshop.js
@@ -1,4 +1,53 @@
 const boules = 'https://www.zeus2025.be/exe/boutique.xml';
+let cart = 0;
+let balls = [];
+
+
+function addToCart() {
+    if (itemStock > 0) {
+        ajouter.disabled = false;
+        ajouter.setAttribute('onclick', 'addToCart()');
+        ajouter.textContent = "Ajouter au panier";
+    } else {
+        ajouter.disabled = true;
+        ajouter.textContent = "Article non disponible";
+    }
+}
+
+function domCreate () {
+    readBoules();
+    let collectionOfArticles = document.getElementById('articles');
+    collectionOfArticles.length = 0;
+    collectionOfArticles.selectedIndex = 0;
+    let boule;
+    for (let i = 0; i < balls.length; i++) {
+        boule = document.createElement('div');
+        boule.setAttribute("class", "article");
+        let itemId = document.createElement('p');
+        itemId.setAttribute("class", "idBoule");
+        itemId = balls[i].id;
+        let itemLib = document.createElement('p');
+        itemLib.setAttribute("class", "libBoule");
+        itemLib = balls[i].lib;
+        let itemPrix = document.createElement('p');
+        itemPrix.setAttribute("class", "prixBoule");
+        itemPrix = balls[i].prix;
+        let itemStock = document.createElement('p');
+        itemStock.setAttribute("class", "libStock");
+        itemStock = balls[i].stock;
+        let itemImage = document.createElement('img');
+        itemImage.setAttribute("class", "imageBoule");
+        itemImage.src = balls[i].image;
+        let ajouter = document.createElement('button');
+        boule.append(itemImage);
+        boule.append(itemLib);
+        boule.append(itemPrix + " €");
+        boule.append(itemStock + " pièces en stock");
+        boule.append(ajouter);
+    }
+    collectionOfArticles.append(boule);
+    console.log(collectionOfArticles);
+}
 
 function readBoules() {
     if (window.XMLHttpRequest) {
@@ -10,34 +59,23 @@ function readBoules() {
     xmlHttp.send();
     xmlDoc = xmlHttp.responseXML;
     let countArticles = xmlDoc.getElementsByTagName('article').length;
-    let collectionOfArticles = document.getElementById('articles');
-    collectionOfArticles.length = 0;
-    collectionOfArticles.selectedIndex = 0;
-    let row;
     let oneBall;
+    let id, lib, prix, stock, image;
     for (let i = 0; i < countArticles; i++) {
-        oneBall = document.createElement('td');
-        oneBall.setAttribute("class", "article");
-        let itemId = document.createElement('p');
-        itemId.setAttribute("class", "idBoule");
-        itemId.innerText = xmlDoc.getElementsByTagName("id")[i].childNodes[0].nodeValue;
-        let itemLib = document.createElement('p');
-        itemLib.setAttribute("class", "libBoule");
-        itemLib = xmlDoc.getElementsByTagName("lib")[i].childNodes[0].nodeValue;
-        let itemPrix = document.createElement('p');
-        itemPrix.setAttribute("class", "prixBoule");
-        itemPrix = xmlDoc.getElementsByTagName("prix")[i].childNodes[0].nodeValue;
-        let itemStock = document.createElement('p');
-        itemStock.setAttribute("class", "libStock");
-        itemStock = xmlDoc.getElementsByTagName("stock")[i].childNodes[0].nodeValue;
-        let itemImage = document.createElement('img');
-        itemImage.setAttribute("class", "imageBoule");
-        itemImage.src = xmlDoc.getElementsByTagName("image")[i].childNodes[0].nodeValue;
-        oneBall.append(itemImage);
-        oneBall.append(itemLib);
-        oneBall.append("\r" + itemPrix + " €");
-        oneBall.append("\r" + itemStock + " pièces en stock");
-        collectionOfArticles.append(oneBall);
+        id = xmlDoc.getElementsByTagName("id")[i].childNodes[0].nodeValue;
+        lib = xmlDoc.getElementsByTagName("lib")[i].childNodes[0].nodeValue;
+        prix = xmlDoc.getElementsByTagName("prix")[i].childNodes[0].nodeValue;
+        stock = xmlDoc.getElementsByTagName("stock")[i].childNodes[0].nodeValue;
+        image = xmlDoc.getElementsByTagName("image")[i].childNodes[0].nodeValue;
+        oneBall = {
+            id,
+            lib,
+            prix,
+            stock,
+            image
+        }
+        balls[i] = oneBall;
     }
+    //console.log(balls);
 }
 
