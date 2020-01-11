c39e04054a7ea1c578b30705068f22d877a4aa5f
FeliciaMarlove <missflorencemary@hotmail.com>
2020-01-11 16:20:50 +0100

    Panier se décrémente.

--- eshop.js
+++ eshop.js
@@ -1,28 +1,24 @@
 const boules = 'https://www.zeus2025.be/exe/boutique.xml';
 let cart = 0;
 let balls = [];
+let collectionOfArticles;
 
-
-function addToCart() {
-    if (itemStock > 0) {
-        ajouter.disabled = false;
-        ajouter.setAttribute('onclick', 'addToCart()');
-        ajouter.textContent = "Ajouter au panier";
-    } else {
-        ajouter.disabled = true;
-        ajouter.textContent = "Article non disponible";
-    }
+function addToCart(index) {
+    let currentBall = document.getElementById("ball"+index);
+    currentBall.childNodes[4].nodeValue -= 1;
 }
+
 
 function domCreate () {
     readBoules();
-    let collectionOfArticles = document.getElementById('articles');
+    collectionOfArticles = document.getElementById('articles');
     collectionOfArticles.length = 0;
     collectionOfArticles.selectedIndex = 0;
     let boule;
     for (let i = 0; i < balls.length; i++) {
         boule = document.createElement('div');
         boule.setAttribute("class", "article");
+        boule.setAttribute("id", "ball" + i)
         let itemId = document.createElement('p');
         itemId.setAttribute("class", "idBoule");
         itemId = balls[i].id;
@@ -39,14 +35,24 @@ function domCreate () {
         itemImage.setAttribute("class", "imageBoule");
         itemImage.src = balls[i].image;
         let ajouter = document.createElement('button');
+        if (itemStock > 0) {
+            ajouter.disabled = false;
+            ajouter.onclick = function() { addToCart(i); };
+            ajouter.textContent = "Ajouter au panier";
+        } else {
+            ajouter.disabled = true;
+            ajouter.textContent = "Article non disponible";
+        }
         boule.append(itemImage);
         boule.append(itemLib);
-        boule.append(itemPrix + " €");
-        boule.append(itemStock + " pièces en stock");
+        boule.append(itemPrix);
+        boule.append(" €");
+        boule.append(itemStock);
+        boule.append(" pièces en stock")
         boule.append(ajouter);
         collectionOfArticles.append(boule);
     }
-
+    console.log('collection d\'articles');
     console.log(collectionOfArticles);
 }
 
@@ -61,15 +67,15 @@ function readBoules() {
     xmlDoc = xmlHttp.responseXML;
     let countArticles = xmlDoc.getElementsByTagName('article').length;
     let oneBall;
-    let id, lib, prix, stock, image;
+    let idd, lib, prix, stock, image;
     for (let i = 0; i < countArticles; i++) {
-        id = xmlDoc.getElementsByTagName("id")[i].childNodes[0].nodeValue;
+        idd = xmlDoc.getElementsByTagName("id")[i].childNodes[0].nodeValue;
         lib = xmlDoc.getElementsByTagName("lib")[i].childNodes[0].nodeValue;
         prix = xmlDoc.getElementsByTagName("prix")[i].childNodes[0].nodeValue;
         stock = xmlDoc.getElementsByTagName("stock")[i].childNodes[0].nodeValue;
         image = xmlDoc.getElementsByTagName("image")[i].childNodes[0].nodeValue;
         oneBall = {
-            id,
+            idd,
             lib,
             prix,
             stock,
